c7d07fefa550b130e6faf68c139b34f8259ebcc8
trexxet <atomnik07@gmail.com>
2021-02-14 03:11:34 +0300

    1.1.1 Add fuel value to NGL

--- AngelBob-Petrochem-Power_1.1.0/data-final-fixes.lua
+++
@@ -1,2 +0,0 @@
-data.raw.fluid["gas-synthesis"].fuel_value = "230kJ"
-data.raw.fluid["gas-synthesis"].emissions_multiplier = 1
---
+++ AngelBob-Petrochem-Power_1.1.1/data-final-fixes.lua
@@ -0,0 +1,4 @@
+data.raw.fluid["gas-synthesis"].fuel_value = "230kJ"
+data.raw.fluid["gas-synthesis"].emissions_multiplier = 1
+data.raw.fluid["liquid-ngl"].fuel_value = "370kJ"
+data.raw.fluid["liquid-ngl"].emissions_multiplier = 1.2
